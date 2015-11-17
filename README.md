9643c258c353da90cfb7ffd18e11e080b28fb966
yusefmarra <yusef.marra@gmail.com>
2015-11-17 11:54:21 -0700

    Initial commit + first story

--- README.md
+++
@@ -1,1 +0,0 @@
-# Node-Express-Socket.io Boilerplate
--- client/public/main.js
+++ client/public/main.js
@@ -1,12 +1,3 @@
 // custom scripts
 
 var socket = io();
-
-$('#socketform').submit(function() {
-  socket.emit('chat message',$('#m').val());
-  $('#m').val('');
-  return false;
-})
-socket.on('chat message', function(msg) {
-  $('#message').append($('<li>').text(msg));
-});
