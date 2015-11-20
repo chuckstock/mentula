62d9b00d7a85fa93df4b975d0c9f867da1101e05
Blackstock <charlieblackstock@gmail.com>
2015-11-20 14:27:39 -0700

    updated viewer for sockets

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -2,10 +2,15 @@
 
 var socket = io();
 
-// Set gameRoom
-var gameRoom = sessionStorage.getItem('game-room') || Math.floor((Math.random() * 10000) + 10000);
-sessionStorage.setItem('game-room', gameRoom);
 
-// Set viewerId
-var viewerId = sessionStorage.getItem('viewer-id') || Math.floor((Math.random() * 10000) + 10000);
-sessionStorage.setItem('viewer-id', viewerId);
+$('#createGame').on('click', function() {
+  // Set gameRoom
+  var gameRoom = sessionStorage.getItem('game-room') || Math.floor((Math.random() * 10000) + 10000);
+  sessionStorage.setItem('game-room', gameRoom);
+
+  // Set viewerId
+  var viewerId = sessionStorage.getItem('viewer-id') || Math.floor((Math.random() * 10000) + 10000);
+  sessionStorage.setItem('viewer-id', viewerId);
+
+
+});
