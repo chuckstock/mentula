7f534b1d27572402b708cdf9e370cc3a65a23790
Blackstock <charlieblackstock@gmail.com>
2015-12-01 10:33:34 -0700

    added debris for each tank and updated particle storm

--- client/public/states/game.js
+++ client/public/states/game.js
@@ -10,28 +10,40 @@ var emitter = null;
 var image = null;
 
 function Game() {
-    this.playerCount;
-    this.gamepads = [];
+  this.playerCount;
+  this.gamepads = [];
 }
 
 Game.prototype = {
   init: function(playerCount) {
-      this.playerCount = playerCount;
-      console.log(playerCount);
-      for (var i = 0; i <= playerCount; i++) {
-          inputs.push({left: 1, right: 1, fire: false});
-      }
-      for (var k = 0; k < game.input.gamepad.padsConnected; k++) {
-          this.gamepads.push({pad: game.input.gamepad['pad'+(k+1)], player: k});
-      }
+    this.playerCount = playerCount;
+    console.log(playerCount);
+    for (var i = 0; i <= playerCount; i++) {
+      inputs.push({left: 1, right: 1, fire: false});
+    }
+    for (var k = 0; k < game.input.gamepad.padsConnected; k++) {
+      this.gamepads.push({pad: game.input.gamepad['pad'+(k+1)], player: k});
+    }
   },
 
   preload: function() {
     game.load.image('land', 'assets/floortile2.png');
     game.load.image('bullet', 'assets/bullet.png');
+
+    //tank debris
+    game.load.image('tank0debris1', 'assets/tank1debris1.png');
+    game.load.image('tank0debris2', 'assets/tank1debris2.png');
+    game.load.image('tank0debris3', 'assets/tank1debris3.png');
     game.load.image('tank1debris1', 'assets/tank1debris1.png');
     game.load.image('tank1debris2', 'assets/tank1debris2.png');
     game.load.image('tank1debris3', 'assets/tank1debris3.png');
+    game.load.image('tank2debris1', 'assets/tank1debris1.png');
+    game.load.image('tank2debris2', 'assets/tank1debris2.png');
+    game.load.image('tank2debris3', 'assets/tank1debris3.png');
+    game.load.image('tank3debris1', 'assets/tank1debris1.png');
+    game.load.image('tank3debris2', 'assets/tank1debris2.png');
+    game.load.image('tank3debris3', 'assets/tank1debris3.png');
+
     game.load.image('tankBurst', 'assets/tank-burst.png');
     game.load.spritesheet('tank0', 'assets/tanksheet.png', 42, 40);
     game.load.spritesheet('tank1', 'assets/tanksheet2.png', 42, 40);
@@ -48,7 +60,7 @@ Game.prototype = {
       inputs[data.player] = data;
     });
     for (var i = 0; i < this.playerCount; i++) {
-        players.push(new Tank(game, i));
+      players.push(new Tank(game, i));
     }
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
@@ -68,51 +80,49 @@ Game.prototype = {
 
     bullets.setAll('filters',[ this.game.add.filter('Glow') ]);
   },
-    update: function() {
-        this.getGamepadInput();
-        for (var i = 0; i < players.length; i++) {
-            // console.log(inputs);
-            players[i].update();
-            game.physics.arcade.overlap(bullets, players[i].sprite, handleBulletCollision, null, this);
-
-        }
-        for (var k = 0; k < players.length; k++) {
-          for (var l = 0; l < players.length; l++) {
-            game.physics.arcade.collide(players[k].sprite, players[l].sprite);
-          }
-        }
-        emitter.forEachAlive(function(p){
-  		    p.alpha= p.lifespan / emitter.lifespan;
-  	    });
-    },
-    getGamepadInput: function() {
-        //   console.log(this.gamepads);
-        for (var i = 0; i < this.gamepads.length; i++) {
-            var pad = this.gamepads[i].pad;
-            var player = this.gamepads[i].player;
-            if (pad.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y) < -0.5) {
-                inputs[player].left = 2;
-            } else if (pad.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y) > 0.5) {
-                inputs[player].left = 0;
-            } else {
-                inputs[player].left = 1;
-            }
-            if (pad.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_Y) < -0.5) {
-                inputs[player].right = 2;
-            } else if (pad.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_Y) > 0.5) {
-                inputs[player].right = 0;
-            } else {
-                inputs[player].right = 1;
-            }
-            if (pad.isDown(Phaser.Gamepad.XBOX360_RIGHT_TRIGGER)) {
-                inputs[player].fire = true;
-            } else {
-                inputs[player].fire = false;
-            }
-
-        }
+  update: function() {
+    this.getGamepadInput();
+    for (var i = 0; i < players.length; i++) {
+      // console.log(inputs);
+      players[i].update();
+      game.physics.arcade.overlap(bullets, players[i].sprite, handleBulletCollision, null, this);
 
     }
+    for (var k = 0; k < players.length; k++) {
+      for (var l = 0; l < players.length; l++) {
+        game.physics.arcade.collide(players[k].sprite, players[l].sprite);
+      }
+    }
+
+  },
+  getGamepadInput: function() {
+    //   console.log(this.gamepads);
+    for (var i = 0; i < this.gamepads.length; i++) {
+      var pad = this.gamepads[i].pad;
+      var player = this.gamepads[i].player;
+      if (pad.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y) < -0.5) {
+        inputs[player].left = 2;
+      } else if (pad.axis(Phaser.Gamepad.XBOX360_STICK_LEFT_Y) > 0.5) {
+        inputs[player].left = 0;
+      } else {
+        inputs[player].left = 1;
+      }
+      if (pad.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_Y) < -0.5) {
+        inputs[player].right = 2;
+      } else if (pad.axis(Phaser.Gamepad.XBOX360_STICK_RIGHT_Y) > 0.5) {
+        inputs[player].right = 0;
+      } else {
+        inputs[player].right = 1;
+      }
+      if (pad.isDown(Phaser.Gamepad.XBOX360_RIGHT_TRIGGER)) {
+        inputs[player].fire = true;
+      } else {
+        inputs[player].fire = false;
+      }
+
+    }
+
+  }
 };
 
 
@@ -128,7 +138,7 @@ function Tank(game, controller) {
   this.colors = [0x00cc00, 0x1a75ff, 0xe5e600, 0xe67300];
   // Controller is the index of input array where this tanks inputs are stored.
   this.controller = controller;
-  this.sprite = game.add.sprite(x, y, 'tank' + (this.controller + 1));
+  this.sprite = game.add.sprite(x, y, 'tank' + (this.controller));
   this.sprite.health = 100;
   this.sprite.frame = 1;
   this.sprite.animations.add('move', [0,1,2], 10, true);
@@ -156,8 +166,18 @@ Tank.prototype = {
     if (this.sprite.health <= 0 && this.sprite.alive) {
       this.sprite.kill();
       this.die();
-      //add explosion animation
+
+      //** Particle Explosion **//
+      var tint = this.sprite.tint;
+      emitter.forEachAlive(function(p){
+        p.alpha = p.lifespan / emitter.lifespan;
+        p.tint = tint;
+        p.scale.setTo(0.5, 0.5);
+      });
+
     }
+
+
     if (this.rotation > 360){
       this.rotation = this.rotation - 360;
     } else if (this.rotation < 0) {
@@ -201,7 +221,7 @@ Tank.prototype = {
     } else if (inputs[this.controller].left === 2 && inputs[this.controller].right === 0) {
       // Left track forward, right track reverse, turn the tank fast right.
       this.sprite.angle += 3;
-      this.rotation += 3
+      this.rotation += 3;
       game.physics.arcade.velocityFromAngle(this.sprite.angle, 0, this.sprite.body.velocity);
       this.sprite.play('move');
     } else if (inputs[this.controller].left === 0 && inputs[this.controller].right === 2) {
@@ -223,13 +243,11 @@ Tank.prototype = {
       if (bullet && this.game.time.now > this.nextFire) {
         //  And fire it
         var radians = this.rotation * (Math.PI/180);
-        // console.log((this.sprite.x + 20) * Math.sin(radians), (this.sprite.y + 20) * Math.cos(radians));
-        // console.log(this.rotation);
         vector = {};
         vector.x = 33 * Math.sin(radians);
         vector.y = 33 * Math.cos(radians);
         if (this.rotation >= 0 && this.rotation <= 90) {
-            bullet.reset(this.sprite.x + vector.x, this.sprite.y - vector.y);
+          bullet.reset(this.sprite.x + vector.x, this.sprite.y - vector.y);
         } else if (this.rotation > 90 && this.rotation <= 180) {
           bullet.reset(this.sprite.x + vector.x, this.sprite.y - vector.y);
         } else if (this.rotation > 180 && this.rotation <= 270) {
@@ -239,10 +257,8 @@ Tank.prototype = {
         }
         bullet.lifespan = 2000;
 
-        // bullet.reset(this.sprite.x, this.sprite.y);
         bullet.angle = this.sprite.angle;
         game.physics.arcade.velocityFromAngle(this.sprite.angle, 400, bullet.body.velocity);
-        // bullet.reset(this.sprite.x, this.sprite.y);
         // console.log(bullet);
 
         this.nextFire = this.game.time.now + this.fireRate;
@@ -250,36 +266,40 @@ Tank.prototype = {
     }
   },
   die: function() {
-      var piece1 = game.add.sprite(this.sprite.x, this.sprite.y, 'tank1debris1');
-      var piece2 = game.add.sprite(this.sprite.x, this.sprite.y, 'tank1debris2');
-      var piece3 = game.add.sprite(this.sprite.x, this.sprite.y, 'tank1debris3');
-      game.physics.enable(piece1, Phaser.Physics.ARCADE);
-      game.physics.enable(piece2, Phaser.Physics.ARCADE);
-      game.physics.enable(piece3, Phaser.Physics.ARCADE);
-      piece1.anchor.set(0.5, 0.5);
-      piece2.anchor.set(0.5, 0.5);
-      piece3.anchor.set(0.5, 0.5);
-      piece1.tint = this.sprite.tint;
-      piece2.tint = this.sprite.tint;
-      piece3.tint = this.sprite.tint;
+    //create sprites for each debris piece
+    var piece1 = game.add.sprite(this.sprite.x, this.sprite.y, 'tank'+ this.controller + 'debris1');
+    var piece2 = game.add.sprite(this.sprite.x, this.sprite.y, 'tank1debris2');
+    var piece3 = game.add.sprite(this.sprite.x, this.sprite.y, 'tank1debris3');
 
-      emitter = game.add.emitter(this.sprite.x, this.sprite.y, 200);
+    //enable physics on each debris piece and set tint
+    game.physics.enable(piece1, Phaser.Physics.ARCADE);
+    game.physics.enable(piece2, Phaser.Physics.ARCADE);
+    game.physics.enable(piece3, Phaser.Physics.ARCADE);
+    piece1.anchor.set(0.5, 0.5);
+    piece2.anchor.set(0.5, 0.5);
+    piece3.anchor.set(0.5, 0.5);
+    piece1.tint = this.sprite.tint;
+    piece2.tint = this.sprite.tint;
+    piece3.tint = this.sprite.tint;
 
-      emitter.makeParticles('tankBurst');
-      emitter.lifespan = 2000
+    //shoot out pieces and specified angle and velocities
+    game.physics.arcade.velocityFromAngle(100, 200, piece1.body.velocity);
+    game.physics.arcade.velocityFromAngle(-85, 200, piece2.body.velocity);
+    game.physics.arcade.velocityFromAngle(0, 200, piece3.body.velocity);
 
-      emitter.start(true, 1000, null, 300);
+    //adds spin to each piece of debris
+    piece1.body.angularVelocity = 800;
+    piece2.body.angularVelocity = 800;
+    piece3.body.angularVelocity = 800;
+
+    //** PARTICLE STROM ON DEATH **//
+    emitter = game.add.emitter(this.sprite.x, this.sprite.y, 200);
+    emitter.makeParticles('tankBurst');
+    emitter.lifespan = 1000;
+    emitter.gravity = 0;
+    emitter.start(true, 750, null, 150);
 
 
-
-
-
-      game.physics.arcade.velocityFromAngle(100, 200, piece1.body.velocity);
-      game.physics.arcade.velocityFromAngle(-85, 200, piece2.body.velocity);
-      game.physics.arcade.velocityFromAngle(0, 200, piece3.body.velocity);
-      piece1.body.angularVelocity = 800;
-      piece2.body.angularVelocity = 800;
-      piece3.body.angularVelocity = 800;
   }
 };
 
@@ -289,32 +309,32 @@ function handleBulletCollision(tank, bullet) {
 }
 
 Phaser.Filter.Glow = function (game) {
-    Phaser.Filter.call(this, game);
+  Phaser.Filter.call(this, game);
 
-    this.fragmentSrc = [
-        "precision lowp float;",
-        "varying vec2 vTextureCoord;",
-        "varying vec4 vColor;",
-        'uniform sampler2D uSampler;',
+  this.fragmentSrc = [
+    "precision lowp float;",
+    "varying vec2 vTextureCoord;",
+    "varying vec4 vColor;",
+    'uniform sampler2D uSampler;',
 
-        'void main() {',
-            'vec4 sum = vec4(0);',
-            'vec2 texcoord = vTextureCoord;',
-            'for(int xx = -4; xx <= 4; xx++) {',
-                'for(int yy = -3; yy <= 3; yy++) {',
-                    'float dist = sqrt(float(xx*xx) + float(yy*yy));',
-                    'float factor = 0.0;',
-                    'if (dist == 0.0) {',
-                        'factor = 2.0;',
-                    '} else {',
-                        'factor = 2.0/abs(float(dist));',
-                    '}',
-                    'sum += texture2D(uSampler, texcoord + vec2(xx, yy) * 0.002) * factor;',
-                '}',
-            '}',
-            'gl_FragColor = sum * 0.025 + texture2D(uSampler, texcoord);',
-        '}'
-    ];
+    'void main() {',
+    'vec4 sum = vec4(0);',
+    'vec2 texcoord = vTextureCoord;',
+    'for(int xx = -4; xx <= 4; xx++) {',
+    'for(int yy = -3; yy <= 3; yy++) {',
+    'float dist = sqrt(float(xx*xx) + float(yy*yy));',
+    'float factor = 0.0;',
+    'if (dist == 0.0) {',
+    'factor = 2.0;',
+    '} else {',
+    'factor = 2.0/abs(float(dist));',
+    '}',
+    'sum += texture2D(uSampler, texcoord + vec2(xx, yy) * 0.002) * factor;',
+    '}',
+    '}',
+    'gl_FragColor = sum * 0.025 + texture2D(uSampler, texcoord);',
+    '}'
+  ];
 };
 
 Phaser.Filter.Glow.prototype = Object.create(Phaser.Filter.prototype);
